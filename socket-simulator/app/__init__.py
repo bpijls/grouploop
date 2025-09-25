5a9126d1c63095efa403d8f95163b66f287ebda1
Bas Pijls <b.pijls@hva.nl>
2025-09-25 15:46:09 +0200

    Refactor Docker Compose and socket-simulator structure; rename services for clarity, adjust port mapping, and reorganize Dockerfile to streamline dependency installation and application setup. Introduce app.py for Flask application entry point and update blueprint registration in __init__.py for consistency.

--- socket-simulator/app/__init__.py
+++ socket-simulator/app/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 def create_app() -> Flask:
 	app = Flask(__name__, static_folder="static", template_folder="templates")
 
-	from .views import bp as main_bp
+	from .views import main_bp
 	app.register_blueprint(main_bp)
 
 	return app
--- socket-simulator/app/views.py
+++ socket-simulator/app/views.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, render_template
 
 
-bp = Blueprint("main", __name__)
+main_bp = Blueprint("main", __name__)
 
 
-@bp.route("/")
+@main_bp.route("/")
 def index():
 	return render_template("index.html")
 
