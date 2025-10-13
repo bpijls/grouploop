aa4b6e87b3e38a0a6daccd06683b14e6e7b3832f
bahaasala <salaymeh1999@live.com>
2025-10-13 12:17:19 +0200

    Enable additional game scenes in socket-demo: Add 'heatmap', 'circles', and 'physics' scenes to the gameStateManager for enhanced gameplay options.

--- socket-demo/sketch.js
+++ socket-demo/sketch.js
@@ -5,9 +5,9 @@ const gameStateManager = new SceneManager(deviceManager);
 // Register example game states
 // gameStateManager.addScene('list', new DeviceListScene(deviceManager));
 // gameStateManager.addScene('first', new FirstDeviceDetailsScene(deviceManager));
-// gameStateManager.addScene('heatmap', new GridHeatmapScene(deviceManager));
-// gameStateManager.addScene('circles', new StateDeviceCirclesScene(deviceManager));
-// gameStateManager.addScene('physics', new PartialPhysicsScene(deviceManager));
+gameStateManager.addScene('heatmap', new GridHeatmapScene(deviceManager));
+gameStateManager.addScene('circles', new StateDeviceCirclesScene(deviceManager));
+gameStateManager.addScene('physics', new PartialPhysicsScene(deviceManager));
 gameStateManager.addScene('physicsWithGlow', new PartialPhysicsWithGlowScene(deviceManager));
 gameStateManager.addScene('wander', new WanderingAttractorsScene(deviceManager));
 
